66269f8994f7f8205337de2d5c69d560ffde30ef
paul <python8u@gmail.com>
2021-07-01 17:36:09 -0400

    setup

---
+++ a.py
@@ -0,0 +1,9 @@
+import numpy as np
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+from time import time
+from torchvision improt datasets, transforms
+from torch import nn, optim
+
+
