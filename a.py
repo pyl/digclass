4564dcf986195541a0e8aea1fda783f2e7e9b1bf
Paul <python8u@gmail.com>
2021-07-18 00:01:59 -0400

    stuff

--- a.py
+++ a.py
@@ -3,8 +3,46 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 from time import time
-from torchvision improt datasets, transforms
+from torchvision import datasets, transforms
 from torch import nn, optim
 
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,)),
+                                ])
 
-print("hi")
+trainset = datasets.MNIST('lib', download=True, train=True, transform=transform)
+valset = datasets.MNIST('lib', download=True, train=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
+
+dataiter = iter(trainloader)
+
+images, labels = dataiter.next()
+
+figure = plt.figure()
+num_of_images = 60
+
+for i in range(1, num_of_images + 1):
+    plt.subplot(6, 10, i)
+    plt.axis('off')
+    plt.imshow(images[i].numpy().squeeze(), cmap='gray_r')
+
+input_size = 784
+hidden_sizes = [128, 64]
+output_size = 10
+
+model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[1], output_size),
+                      nn.LogSoftmax(dim=1))
+print(model)
+
+criterion = nn.NLLLoss()
+images, labels = next(iter(trainloader))
+images = images.view(images.shape[0], -1)
+
+
+
+plt.show()
